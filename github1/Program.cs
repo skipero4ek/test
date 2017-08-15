eaeb0f3c619d55f2f85e8abbfc657da738708e41
Skipero4ek <toxt@mail.ru>
2017-08-15 10:15:48 +0300

    test commit

---
+++ github1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace github1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create(".gitignore");
+            Console.WriteLine("Hello, gitHUB.com!!!! ");
+        }
+    }
+}
